be99b443fb9b8caf183e6434979bfc17a0085f63
Annette Kariuki <kariukian1@gmail.com>
2023-09-07 18:57:04 +0300

    Complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
+
+  //using the useState hook to initialize the timeRemaining and giving it an initial value of 10
+  
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  //starting our useEffect hook so that we can run side effects as we go.
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    //setting setTimeout that will essentially run a callback function after 1000 milliseconds.
+    //also the timeRemaining shall decrease by 1 (from the deliverables given)
+    const timerId = setTimeout(() => {
+      
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    
+
+    //setting up the cleanUp now. So when we're done with the component it's unmounted. 
+    //so this is where we clear out any timeouts or intervals that were created in the past.
+    
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
